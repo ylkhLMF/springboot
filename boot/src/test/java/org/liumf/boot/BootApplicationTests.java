11cba718fb43b346a82c750d4fd5933721c09c48
liumf <liumf@vip.qq.com>
2019-12-24 18:21:14 +0800

    添加pom,整合jdbc

--- boot/src/test/java/org/liumf/boot/BootApplicationTests.java
+++ boot/src/test/java/org/liumf/boot/BootApplicationTests.java
@@ -1,13 +1,28 @@
 package org.liumf.boot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootTest
 class BootApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
+
+        System.out.println(dataSource.getClass());
+
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection.toString());
+        connection.close();
+        System.out.println("111");
     }
 
 }
